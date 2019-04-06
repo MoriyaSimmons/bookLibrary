dc9f64a7e7c924e6c85c5a3084fefdb1b3d3309f
MoriyaSimmons <48654200+MoriyaSimmons@users.noreply.github.com>
2019-04-06 21:48:47 +0300

    Update add-book.component.ts

--- src/app/add-book/add-book.component.ts
+++ src/app/add-book/add-book.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddBookComponent implements OnInit {
 
-  constructor() { }
+   @Input() book: Book;
+
+  constructor(private router: Router, private ser: LibraryServiceService ) {
+  }
 
   ngOnInit() {
+  }
+
+  addButton(title: string, author: string, date: string) {
+    if (title.length == 0 || author.length == 0 || date.length == 0) {
+      alert('error, please enter a valid value');
+    } else {
+      this.book = new Book(this.bookService.getBooks().length + 2, author, new Date(), title); 
+      this.ser.addBook(this.book);
+      this.router.navigate(['/library']);
+    }
   }
 
 }
